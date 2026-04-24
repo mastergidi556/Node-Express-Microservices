97cf452ef5f4438293818d203bdcd46e3c415cbf
Okikiayo Gidi-Fanimokun <mastergidi556@gmail.com>
2026-04-24 07:12:53 +0100

    updated deploy worflow env

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,7 @@ resource "aws_instance" "microservices_vm" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = var.instance_type
   key_name      = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [
     aws_security_group.microservices_sg.id
